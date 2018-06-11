b47147c8162eba44573ef7917f4182b9e531df04
pl8848 <playdone@163.com>
2018-06-11 17:02:45 +0800

    frist

---
+++ sh/lamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#全新电脑全新安装 lamp
+#install lamp
+apt-get update && apt-get upgrade
+
+~                                                                                                                                           
+~                                                
